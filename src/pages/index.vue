339f4a48622242490116435717cbba36d7365ce7
lala-haha74 <1170485145@qq.com>
2023-03-25 12:48:29 +0800

    第一次提交说明信息

---
+++ src/pages/index.vue
@@ -0,0 +1,173 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+import type { BlockState } from '~/types'
+
+const WIDTH = 5
+const HEIGHT = 5
+const state = reactive(
+  Array.from({ length: HEIGHT }, (_, y) =>
+    Array.from({ length: WIDTH },
+      (_, x): BlockState => ({
+        x,
+        y,
+        adjacentMines: 0,
+      }),
+    ),
+  ),
+)
+
+function generateMines(initial: BlockState) {
+  for (const row of state) {
+    for (const block of row) {
+      if (Math.abs(initial.x - block.x) <= 1)
+        continue
+      if (Math.abs(initial.y - block.y) <= 1)
+        continue
+      block.mine = Math.random() < 0.5
+    }
+  }
+  updateNumbers()
+}
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+function updateNumbers() {
+  state.forEach((raw, y) => {
+    raw.forEach((block, x) => {
+      if (block.mine)
+        return
+      getSiblings(block)
+        .forEach((b) => {
+          if (b.mine)
+            block.adjacentMines += 1
+        })
+    })
+  })
+}
+
+function expendZero(block: BlockState) {
+  if (block.adjacentMines)
+    return
+
+  getSiblings(block).forEach((s) => {
+    if (!s.revealed) {
+      s.revealed = true
+      expendZero(s)
+    }
+  })
+}
+
+let mineGenerated = false
+const dev = false
+
+function onRightClick(block: BlockState) {
+  if (block.revealed)
+    return
+  block.flagged = !block.flagged
+  // checkGameState()
+}
+function onClick(e: MouseEvent, block: BlockState) {
+  if (!mineGenerated) {
+    generateMines(block)
+    mineGenerated = true
+  }
+
+  block.revealed = true
+  if (block.mine)
+    alert('BOOM!!!!!!!!!!!!!!!!!')
+  expendZero(block)
+  // checkGameState()
+}
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-orange-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-pink-500',
+]
+
+function getBlockClass(block: BlockState) {
+  if (block.flagged)
+    return 'bg-gray-500/10'
+  if (!block.revealed)
+    return 'bg-gray-500/10 hover:bg-gray-500/20'
+
+  return block.mine
+    ? 'bg-red-500/50'
+    : numberColors[block.adjacentMines]
+}
+
+function getSiblings(block: BlockState) {
+  return directions.map(([dx, dy]) => {
+    const x2 = block.x + dx
+    const y2 = block.y + dy
+    if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+      return undefined
+
+    return state[y2][x2]
+  })
+    .filter(Boolean) as BlockState[]
+}
+
+function checkGameState() {
+  if (!mineGenerated)
+    return
+  const blocks = state.flat()
+
+  if (blocks.every(block => block.revealed || block.flagged)) {
+    if (blocks.some(block => block.flagged && !block.mine))
+      alert('You cheat!')
+    else
+      alert('You win!')
+  }
+}
+watchEffect(checkGameState)
+</script>
+
+<template>
+  <div>
+    Minesweeper
+    <div p5>
+      <div
+        v-for="row, y in state"
+        :key="y"
+        flex="~"
+        items-center justify-center
+      >
+        <button
+          v-for="block, x in row"
+          :key="x"
+          flex="~"
+          items-center justify-center
+          w-8 h-8 m="0.5"
+          border="2 gray-400/10"
+          :class="getBlockClass(block)"
+          @click="onClick($event, block)"
+          @contextmenu.prevent="onRightClick(block)"
+        >
+          <template v-if="block.flagged">
+            <div i-mdi-flag text-red />
+          </template>
+          <template v-else-if="block.revealed || dev">
+            <div v-if="block.mine" i-mdi-mine>
+              x
+            </div>
+            <div v-else>
+              {{ block.adjacentMines }}
+            </div>
+          </template>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
